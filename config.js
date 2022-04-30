a6f5dd4be69ef7317f5b2a8f678c65580f8487ff
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-04-30 19:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285785694474', '6285785694474','6285828764046','6285828764046','6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6281322577123', '6281322577123','6281322577123','6281322577123','6281322577123'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -40,8 +40,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Rzy-bot'
-global.author = 'Rzy-bot'
+global.packname = 'KAMAGAPO BOT'
+global.author = 'KAMAGAPO BOT'
 
 global.wait = '「 ⏱️ 」Harap tunggu sebentar...'
 global.eror = '「❗」Server ERROR!'
